693e524b9166534e5e26b140ac74e873cc7dc682
pdubb22 <pgwheeler@cox.net>
2018-05-24 21:05:54 -0500

    first api call

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpfactoryService } from '../httpfactory.service';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  
+
+  constructor(private httpfactoryservice: HttpfactoryService) {
+  }
+  
+    
+  retrievestudent(){
+    this.httpfactoryservice.retrievestudent().subscribe(res=>{
+      console.dir(res)
+    })
+  }
+
+  ngOnInit() {
+  }
+
+
+}
+
+
